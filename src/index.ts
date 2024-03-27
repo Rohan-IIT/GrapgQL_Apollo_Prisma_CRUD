20bf06a7988e09d0f82e4f6623d0ff3165688a9b
Rohan-IIT <rpawar3@hawk.iit.edu>
2024-03-27 16:38:25 -0500

    prisma added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const app = express();
@@ -11,11 +12,42 @@ async function init() {
     typeDefs: `
     type Query {
         hello: String
-    }`,
+    }
+    type Mutation {
+      createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+    }
+    `,
     resolvers: {
-        Query: {
-            hello: () => `Hey there`
-        }
+      Query: {
+        hello: () => `Hey there`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "random_salt",
+            },
+          });
+          return true;
+        },
+      },
     },
   });
   await gqlserver.start();
